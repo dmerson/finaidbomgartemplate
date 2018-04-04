6a9a08a5f3f263164bb7283cd5e7ed94ebd2630c
dmerson <dmerson@email.arizona.edu>
2018-04-04 10:36:19 -0700

    Added arrary of date checker.

--- checkOsfaStatus.js
+++ checkOsfaStatus.js
@@ -1,34 +1,50 @@
-function checkOsfaStatus(textDay) {
+function checkOsfaStatus(textDay, arrayOfClosedDates) {
     var today = new Date(textDay);
     var isChatEnabled = true;
     var monthday = (today.getMonth() + 1) + "/" + today.getDate();
     var todaysHour = today.getHours();
     var todaysDay = today.getDay();
     var minutenow = today.getMinutes();
-    if (todaysDay == 0 || todaysDay == 6) { //if todays is  saturday or sunday
-        isChatEnabled = false;
+
+
+    if (arrayOfClosedDates != undefined) {
+        lngOfArray = arrayOfClosedDates.length;
+        console.log("length of array is " + lngOfArray);
+        console.log("day to prepare " + monthday);
+        for (i = 0; i < lngOfArray; i++) {
+            console.log(arrayOfClosedDates[i]);
+            if (arrayOfClosedDates[i] == monthday) {
+                return false;
+            }
+        }
     } else {
+        console.log("No array of dates passed");
+    }
+    
+    if (todaysDay == 0 || todaysDay == 6) { //if todays is  saturday or sunday
+        console.log('sat or sun');
+        return false;
+    } else { // weekdays
 
         if (todaysHour == 8 && todaysDay == 2) { //we are not open on tuesday at 8
-            isChatEnabled = false;
-        } else {
+            console.log('tuesday at 8AM');
+            return false;
+        } else { //other weekdays
 
             if (todaysHour >= 15 || todaysHour < 8) { //if it is before or on 8 or after 4
                 if (todaysHour == 15) { // if it is 4 PM
                     if (minutenow < 30) { //if it is less than 4:30 PM
-                        isChatEnabled = true;
+                        console.log("4 but not past 29 minutes")
+                        return true;
                     } else {
-                        isChatEnabled = false;
+                        return false;
                     }
 
-                } else { //9-3pm
-
-                    isChatEnabled = true;
-
-
                 }
+                 
             }
         }
+        
     }
     return isChatEnabled;
 
